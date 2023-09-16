cccbad1fee1167f2de5110ebc90abe93c1572d3f
ForcePalm <48830341+ForcePalm@users.noreply.github.com>
2023-09-16 02:32:03 +0200

    gulp update for js

--- www/app/gulpfile.js
+++ www/app/gulpfile.js
@@ -2,15 +2,17 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const watch = require('gulp-watch');
+const uglify = require('gulp-uglify');
 
 let isMinifying = false;
+let isMinifyingJS = false;
 
 // Define a task to minify CSS and rename the output file
 gulp.task('minify-css', () => {
   if (!isMinifying) {
     isMinifying = true;
 
-  return gulp.src('webroot/css/styles.css') // Path to your CSS files
+  return gulp.src(['webroot/css/**/*.css', '!webroot/css/**/*.min.css']) // Path to your CSS files
     .pipe(cleanCSS())
     .pipe(rename('styles.min.css')) // Rename the output file
     .pipe(gulp.dest('webroot/css')) // Output directory for the renamed file
@@ -20,9 +22,24 @@ gulp.task('minify-css', () => {
   }
 });
 
+// Define a task to minify JavaScript
+gulp.task('minify-js', () => {
+  if (!isMinifyingJS) {
+    isMinifyingJS = true;
+
+    return gulp.src(['webroot/js/**/*.js', '!webroot/js/**/*.min.js'])
+      .pipe(uglify())
+      .pipe(rename('scripts.min.js'))
+      .pipe(gulp.dest('webroot/js'))
+      .on('end', () => {
+        isMinifyingJS = false; // Reset the flag after the task is done
+      });
+  }
+});
+
 // Define a watch task using gulp-watch with specific events
 gulp.task('watch', () => {
-  watch('webroot/css/**/*.css', { events: ['add', 'change', 'unlink'] }, (file) => {
+  watch(['webroot/css/**/*.css', '!webroot/css/**/*.min.css'], { events: ['add', 'change', 'unlink'] }, (file) => {
     const filePath = file.relative;
     // Check if the file path is not the path of the minified file
     if (filePath !== 'styles.min.css') {
@@ -31,7 +48,14 @@ gulp.task('watch', () => {
     }
 
   });
+
+  watch(['webroot/js/**/*.js', '!webroot/js/**/*.min.js'], { events: ['add', 'change', 'unlink'] }, (file) => {
+    const filePath = file.relative;
+    if (filePath !== 'scripts.min.js') {
+      gulp.series('minify-js')();
+    }
+  });
 });
 
 // Define a default task to run when you run 'gulp' without arguments
-gulp.task('default', gulp.series('minify-css', 'watch'));
+gulp.task('default', gulp.series('minify-css', 'minify-js', 'watch'));
